ec248d938b539633fc453b6c0c0a767ad6e6961f
Perrine Lardilleux <perrinelardilleux@gmail.com>
2021-05-26 18:02:25 -0300

    simple airbnb unfinished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  flat = Flat.new(
+    name: Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description:  Faker::Lorem.paragraph,
+    price_per_night: "#{rand(40..100)} $",
+    number_of_guest: rand(0..6)
+  )
+  flat.save!
+end
